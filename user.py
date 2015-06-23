062371eaadec9603b82f9e1557ce9c23ad94d1d4
ogawa <ogawa@oga-pc04.(none)>
2015-06-24 06:32:48 +0900

    update

--- user.py
+++ user.py
@@ -25,21 +25,21 @@ def init(robotHost=None):
     connectComps()
 
     print "activating components"
-    #activateComps(rtcList)
+    activateComps(rtcList)
 
     print "initialized successfully"
 
 def activateComps(rtcList):
     rtm.serializeComponents(rtcList)
-    for r in rtcList:
-        r.start()
+    #for r in rtcList:
+    #    r.start()
 
 def initRTC(module, name):
     ms.load(module)
     return ms.create(module, name)
 
 def createComps(hostname=socket.gethostname()):
-    global ms, adm_svc, rh, rh_svc, servo, seq, seq_svc, kf, simulation_mode
+    global ms, adm_svc, rh, rh_svc, servo, seq, seq_svc, kf, sh, simulation_mode
     ms = rtm.findRTCmanager(hostname)
     rh = rtm.findRTC("RobotHardware0")
     if rh != None:
@@ -59,15 +59,25 @@ def createComps(hostname=socket.gethostname()):
     seq_svc = OpenHRP.creekSequencePlayerServiceHelper.narrow(seq.service("service0"))
 
     kf  = initRTC("creekStateEstimator", "kf")
+    sh  = initRTC("creekReferenceHolder", "holder")
 
-    return [rh, kf]
+    return [rh, seq, kf, sh]
 
 def connectComps():
+    rtm.connectPorts(rh.port("qCur"),   sh.port("qCur"))
+    rtm.connectPorts(sh.port("qOut"),   servo.port("qRef"))
+
     rtm.connectPorts(rh.port("gyrometer"), kf.port("rate"))
     rtm.connectPorts(rh.port("gsensor"),   kf.port("acc"))
 
-    rtm.connectPorts(rh.port("qCur"),   seq.port("qInit"))
-    rtm.connectPorts(seq.port("qRef"),  servo.port("qRef"))
+    rtm.connectPorts(sh.port("qOut"),        seq.port("qInit"))
+    rtm.connectPorts(sh.port("basePosOut"),  seq.port("basePosInit"))
+    rtm.connectPorts(sh.port("baseRpyOut"),  seq.port("baseRpyInit"))
+    rtm.connectPorts(sh.port("zmpRefOut"),   seq.port("zmpRefInit"))
+    rtm.connectPorts(seq.port("qRef"),     sh.port("qIn"))
+    rtm.connectPorts(seq.port("basePos"),  sh.port("basePosIn"))
+    rtm.connectPorts(seq.port("baseRpy"),  sh.port("baseRpyIn"))
+    rtm.connectPorts(seq.port("zmpRef"),   sh.port("zmpRefIn"))
 
 def setupLogger():
     print "dummy"
@@ -76,13 +86,29 @@ def saveLog(fname='sample'):
     print 'saved'
 
 def goInital():
-    seq_svc.setJointAngles(bodyinfo.makeCommandPose(bodyinfo.initialPose), bodyinfo.timeToInitialPose)
+    tm = bodyinfo.timeToInitialPose
+    seq_svc.setJointAngles(bodyinfo.makeCommandPose(bodyinfo.initialPose), tm)
+    seq_svc.setBasePos([0.0, 0.0, 0.8530], tm)
+    seq_svc.setZmp([0.0, 0.0, -0.8530], tm)
+    seq_svc.waitInterpolation()
 
 def goHalfSitting():
-    seq_svc.setJointAngles(bodyinfo.makeCommandPose(bodyinfo.halfsitPose), bodyinfo.timeToHalfsitPose)
+    tm = bodyinfo.timeToHalfsitPose
+    seq_svc.setJointAngles(bodyinfo.makeCommandPose(bodyinfo.halfsitPose), tm)
+    seq_svc.setBasePos([0.0274, 0.0, 0.7879], tm)
+    seq_svc.setZmp([0.0274, 0.0, -0.7879], tm)
+    seq_svc.waitInterpolation()
 
 def test():
-    seq_svc.isEmpty()
+    pose = [ -30, 0, 0, 0, 0, 0,
+              -30, 0, 0, 0, 0, 0,
+              0, 0, 0,
+              0, 0, 0,
+              10, -5, 0, -30, 0, 0, 0,
+              0, 0, 0, 0, 0, 0,
+              10,  5, 0, -30, 0, 0, 0,
+              0, 0, 0, 0, 0, 0 ]
+    seq_svc.setJointAngles(bodyinfo.makeCommandPose(pose), 5.0)
 
 
 if __name__ == '__main__' or __name__ == 'main':
